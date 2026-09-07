371ec43cc058b58a8ce0ab9a76eaeb22a6e4fd63
Lobo Blanco <admin@roleplus.app>
2026-09-07 10:18:04 +0200

    fix(cache): forzar la limpieza de caché del navegador para restaurar el masonry del timeline a todos los usuarios

--- private/libs/app_controller.php
+++ private/libs/app_controller.php
@@ -36,7 +36,7 @@ abstract class AppController extends GeneralController
         // 4) Datos comunes de vistas
         $this->claves  = (new Configuracion)->todas();
         $this->usuario = (new Usuarios)->uno();
-        $this->version = '2609061132';
+        $this->version = '2609071018';
 
         // 5) CRÍTICO: liberar el lock de sesión antes de renderizar
         Session::close(); // equivale a session_write_close() si está abierta
