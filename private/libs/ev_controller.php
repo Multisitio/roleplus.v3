2f4757a9ec2dc17f598efb69fc0fa2dbedce447d
Lobo Blanco <admin@roleplus.app>
2026-09-11 17:39:11 +0200

    fix: reemplazar time() por version en estáticos para cache busting controlado

--- private/libs/ev_controller.php
+++ private/libs/ev_controller.php
@@ -35,7 +35,7 @@ class EvController extends GeneralController
 
 		$this->claves	= (new Configuracion)->todas();
 		$this->usuario	= (new Usuarios)->uno();
-        $this->version = '2609061933'; # datetime
+        $this->version = '2609111738'; # datetime
 
 		# CRÍTICO rendimiento:
 		# liberamos el lock de la sesión aquí para que esta petición
