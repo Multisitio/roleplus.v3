a281709b13c8ae122efb3f9f63a8ea32436a8707
Lobo Blanco <admin@roleplus.app>
2026-09-12 14:14:45 +0200

    fix(css): mantener main>div con fondo solido y hacer article transparente para corregir bounding box scale

--- private/libs/ev_controller.php
+++ private/libs/ev_controller.php
@@ -35,7 +35,7 @@ class EvController extends GeneralController
 
 		$this->claves	= (new Configuracion)->todas();
 		$this->usuario	= (new Usuarios)->uno();
-        $this->version = '2609121409'; # datetime
+        $this->version = '2609121414'; # datetime
 
 		# CRÍTICO rendimiento:
 		# liberamos el lock de la sesión aquí para que esta petición
