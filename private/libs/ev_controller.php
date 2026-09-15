edb3121e61fe4a3beca9f8642b0b59af369dc308
Lobo Blanco <admin@roleplus.app>
2026-09-15 11:14:57 +0200

    Minificado CSS editor y actualización caché version

--- private/libs/ev_controller.php
+++ private/libs/ev_controller.php
@@ -39,7 +39,7 @@ class EvController extends GeneralController
 
 		$this->claves	= (new Configuracion)->todas();
 		$this->usuario	= (new Usuarios)->uno();
-        $this->version = '2609141910'; # datetime
+        $this->version = '2609151114'; # datetime
 
 		# CRÍTICO rendimiento:
 		# liberamos el lock de la sesión aquí para que esta petición
