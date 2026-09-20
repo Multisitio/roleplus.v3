d420aac1548a485306e70a0549937d814bd97ebb
Lobo Blanco <admin@roleplus.app>
2026-09-20 10:11:50 +0200

    Fix UI glitches and add top margin to editor

--- private/libs/ev_controller.php
+++ private/libs/ev_controller.php
@@ -39,7 +39,7 @@ class EvController extends GeneralController
 
 		$this->claves	= (new Configuracion)->todas();
 		$this->usuario	= (new Usuarios)->uno();
-        $this->version = '2609161642'; # datetime
+        $this->version = '2609201015'; # datetime
 
 		# CRÍTICO rendimiento:
 		# liberamos el lock de la sesión aquí para que esta petición
