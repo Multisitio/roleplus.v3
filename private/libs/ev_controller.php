f13644fdf9bac5506750e63ca6bed4ee7c187409
Lobo Blanco <admin@roleplus.app>
2026-09-20 11:02:20 +0200

    Fix CSS color for trash buttons (apply filter to SVG img)

--- private/libs/ev_controller.php
+++ private/libs/ev_controller.php
@@ -39,7 +39,7 @@ class EvController extends GeneralController
 
 		$this->claves	= (new Configuracion)->todas();
 		$this->usuario	= (new Usuarios)->uno();
-        $this->version = '2609201056'; # datetime
+        $this->version = '2609201101'; # datetime
 
 		# CRÍTICO rendimiento:
 		# liberamos el lock de la sesión aquí para que esta petición
