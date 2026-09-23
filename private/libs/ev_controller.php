6e8a07e7e55c90bc558872eb051261848f72daa1
Lobo Blanco <admin@roleplus.app>
2026-09-23 11:06:43 +0200

    Excluir button-floating del cursor text en el editor

--- private/libs/ev_controller.php
+++ private/libs/ev_controller.php
@@ -39,7 +39,7 @@ class EvController extends GeneralController
 
 		$this->claves	= (new Configuracion)->todas();
 		$this->usuario	= (new Usuarios)->uno();
-        $this->version = '2609230929'; # datetime
+        $this->version = '2609231105'; # datetime
 
 		# CRÍTICO rendimiento:
 		# liberamos el lock de la sesión aquí para que esta petición
